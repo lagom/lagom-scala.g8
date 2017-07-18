14722c72d2ab9ab5cdc72afca166a7af64f74616
Tim Moore <tim.moore@lightbend.com>
2017-07-18 12:22:24 +0930

    Update describeServices to describeService
    We now get a deprecation warning when describeServices is used.
    
    Fixes #13

--- src/main/g8/$name__norm$-impl/src/main/scala/$package$/impl/$name__Camel$Loader.scala
+++ src/main/g8/$name__norm$-impl/src/main/scala/$package$/impl/$name__Camel$Loader.scala
@@ -20,9 +20,7 @@ class $name;format="Camel"$Loader extends LagomApplicationLoader {
   override def loadDevMode(context: LagomApplicationContext): LagomApplication =
     new $name;format="Camel"$Application(context) with LagomDevModeComponents
 
-  override def describeServices = List(
-    readDescriptor[$name;format="Camel"$Service]
-  )
+  override def describeService = Some(readDescriptor[$name;format="Camel"$Service])
 }
 
 abstract class $name;format="Camel"$Application(context: LagomApplicationContext)
