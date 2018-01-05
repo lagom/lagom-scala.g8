f87430387a6a771ef843a8a046407321e07e662e
Renato Cavalcanti <renato@lightbend.com>
2018-01-05 10:40:06 +0100

    [WIP] Defaulting to Lagom 1.4.0-RC1 (#23)
    Defaulting to Lagom 1.4.0-RC1

--- src/main/g8/$name__norm$-api/src/main/scala/$package$/api/$name__Camel$Service.scala
+++ src/main/g8/$name__norm$-api/src/main/scala/$package$/api/$name__Camel$Service.scala
@@ -44,7 +44,7 @@ trait $name;format="Camel"$Service extends Service {
         pathCall("/api/hello/:id", useGreeting _)
       )
       .withTopics(
-        topic($name;format="Camel"$Service.TOPIC_NAME, greetingsTopic _)
+        topic($name;format="Camel"$Service.TOPIC_NAME, greetingsTopic)
           // Kafka partitions messages, messages within the same partition will
           // be delivered in order, to ensure that all messages for the same user
           // go to the same partition (and hence are delivered in order with respect
